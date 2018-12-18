5211862b9e0462bfd1c0df88f38852a5125010df
lanbin <lanbin1987@gmail.com>
2018-12-18 20:42:00 +0800

    add ListNode Model and tools

--- src/mergeTwoLists21.js
+++ src/mergeTwoLists21.js
@@ -2,7 +2,7 @@
  * @Author: lanbin
  * @Date:   2018-12-17 19:48:15
  * @Last Modified by:   lanbin
- * @Last Modified time: 2018-12-17 19:55:55
+ * @Last Modified time: 2018-12-18 20:41:37
  */
 
 /**
@@ -13,8 +13,28 @@
  * }
  */
 
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+/**
+ * 链表只要返回第一个头，后面按照一个接一个的拼接好就行了
+ */
 var mergeTwoLists = function(l1, l2) {
-    console.log('123')
+    if (!l1) return l2
+    if (!l2) return l1
+    let res = null
+
+    if (l1.val < l2.val) {
+        res = l1
+        res.next = mergeTwoLists(l1.next, l2)
+    } else {
+        res = l2
+        res.next = mergeTwoLists(l1, l2.next)
+    }
+    return res
 };
 
 export default mergeTwoLists
---
+++ utils/list.js
@@ -0,0 +1,73 @@
+/*
+ * @Author: lanbin
+ * @Date:   2018-12-18 11:03:16
+ * @Last Modified by:   lanbin
+ * @Last Modified time: 2018-12-18 20:23:52
+ */
+
+import columnify from 'columnify'
+
+class ListNode {
+    constructor(val = '', next = null) {
+        this.val = val
+        this.next = next
+    }
+}
+
+const List = {
+
+    // 这里目前实现的都是单向不循环的链表
+
+    /**
+     * [from 将数组转成链表]
+     * @param  {Array} data [数据源]
+     * @return {ListNode}      [链表的头]
+     */
+    from(data) {
+        let current = null
+        let next = null
+
+        if (Array.isArray(data)) {
+            for (var index = data.length - 1; index >= 0; index--) {
+                current = new ListNode(data[index], current)
+            }
+        }
+        return current
+    },
+
+    /**
+     * [display 显示链表的顺序，显示值]
+     * @param  {ListNode} node [链表的某个元素]
+     */
+    display(node) {
+        let val = []
+        while (node) {
+            val.push(node.val)
+            node = node.next
+        }
+        console.log(val.join('->'))
+    },
+
+    /**
+     * [compare 对比两个链表]
+     * @param  {List} l1 [链表]
+     * @param  {List} l2 [链表]
+     * @return {Boolean}    [是否相等]
+     */
+    compareValue(l1, l2) {
+        while (l1) {
+            // 如果l2没有了 则false
+            if (!l2) return false
+            // 如果值不一样 则false
+            if (l1.val !== l2.val) return false
+            l1 = l1.next
+            l2 = l2.next
+        }
+
+        // l1匹配结束之后 l2也应该结束
+        return !l1 && !l2
+    }
+
+}
+
+export default List
